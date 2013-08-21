402df6bbd6ec36ba5bf841a553134ebe75dbe85c
Simon Riley <syriley@gmail.com>
2013-08-21 17:41:00 +0100

    jumbotron added

--- wordpress/wp-content/themes/twentythirteen/page-templates/app.php
+++ wordpress/wp-content/themes/twentythirteen/page-templates/app.php
@@ -12,6 +12,14 @@
 <?php get_header() ?>
 <div class="container">
   <div class="wrapper">
+  <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to Spotifair!</h1>
+        <p>We may have to change the name for legal reasons at some point, but we're here to 
+          help revive the music industry and get the money back to the music makers.
+        </p>
+      </div>
+    </div>
     <section class="playlistRegion"></section>
     <section class="videoPlayerRegion"></section>
   </div>
